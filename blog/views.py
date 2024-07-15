7c3aa1fe5c223f2ff9c4ca5bad8988edff15471c
Vaishnavipol <vaishnavipol2709@gmail.com>
2024-07-15 10:09:00 +0530

    create base file into index file(html)

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render # type: ignore
 
 # Create your views here.
+
+def starting_page(request):
+    return render(request,"blog/index.html")
+
+def posts(request):
+    pass
+
+def post_detail(request):
+    pass
+
+
