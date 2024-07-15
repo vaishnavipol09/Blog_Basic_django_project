c785ae9770ef7ab21442da6fcfb7ba91e6d15556
Vaishnavipol <vaishnavipol2709@gmail.com>
2024-07-15 16:08:48 +0530

    basic_project_successfully

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,22 @@
+from datetime import date
+
 from django.shortcuts import render # type: ignore
+
+posts=[
+    {
+        "slug": "hike-in-the-mountains",
+        "images": "django_3.jpg",
+        "author": "Vaishnavi",
+        "date": date(2024, 7, 15),
+        "title": "Mountain Hiking",
+        "excerpt" : "Mountains are prominent landforms formed by tectonic forces or volcanism, offering diverse ecosystems and stunning landscapes.",
+        "content": """
+                    Mountains are prominent landforms formed by tectonic forces or volcanism, offering diverse ecosystems and stunning landscapes
+                    Mountains are prominent landforms formed by tectonic forces or volcanism, offering diverse ecosystems and stunning landscapes
+                    Mountains are prominent landforms formed by tectonic forces or volcanism, offering diverse ecosystems and stunning landscapes
+"""
+    }
+]
 
 # Create your views here.
 
@@ -8,7 +26,7 @@ def starting_page(request):
 def posts(request):
     return render(request,"blog/all_posts.html")
 
-def post_detail(request):
-    pass
+def post_detail(request, slug):
+    return render(request,"blog/post_detail.html")
 
 
